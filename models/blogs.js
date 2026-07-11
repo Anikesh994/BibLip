357af42b67c30942602d66900f3af66fb78f5264
Anikesh994 <anikeshkumar994@gmail.com>
2026-07-11 21:41:58 +0530

    blogs

---
+++ models/blogs.js
@@ -0,0 +1,27 @@
+const {Schema ,model} = require("mongoose");
+const { applyTimestamps } = require("./user");
+
+const blogSchema = new Schema({
+    title : {
+        type : String,
+        require : true,
+    },
+    body : {
+        type : String,
+        required : true,
+    },
+    coverImageUrl : {
+        type : String,
+        required : false,
+    },
+
+    createdBy : {
+        type : Schema.Types.ObjectId,
+        ref : "user",
+    },
+
+} ,{timestamps : true});
+
+const Blog = model("blog" ,blogSchema);
+
+module.exports = Blog;
